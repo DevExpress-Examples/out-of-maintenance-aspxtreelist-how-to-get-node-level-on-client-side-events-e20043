d0dac81ab34e45ec883b62400f41244bb1c7d8d7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxTreeList;
+using DevExpress.Web.ASPxEditors;
+using DevExpress.Web.ASPxClasses;
+using System.Collections;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void treeList_DataBound(object sender, EventArgs e)
+    {
+        ArrayList level2Nodes = new ArrayList();
+        TreeListNodeIterator iterator = new TreeListNodeIterator(treeList.RootNode);
+        while (iterator.Current != null)
+        {
+            if (iterator.Current != treeList.RootNode)
+                if (iterator.Current.Level == 2)
+                    level2Nodes.Add(iterator.Current.Key);
+            iterator.GetNext();
+        }
+
+        treeList.JSProperties.Add("cp_level2Nodeslist", level2Nodes);
+    }
+}
---
+++ VB/WebSite/Default2.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="vb" AutoEventWireup="true" CodeFile="Default2.aspx.vb" Inherits="Default2" %>
+
+<%@ Register Assembly="DevExpress.Web.ASPxGridView.v12.1, Version=12.1.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+	Namespace="DevExpress.Web.ASPxGridView" TagPrefix="dx" %>
+<%@ Register Assembly="DevExpress.Web.ASPxEditors.v12.1, Version=12.1.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+	Namespace="DevExpress.Web.ASPxEditors" TagPrefix="dx" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div id="div1" runat="server">
+	</div>
+	</form>
+</body>
+</html>
